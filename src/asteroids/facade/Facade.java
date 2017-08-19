8b3c01c5893eae6c5600768e42d5eed08bc2ce38
Allen <kevertje7@hotmail.com>
2017-08-19 17:42:45 +0200

    Made exception, made xVelocity, YVelocity, update Facade

--- src/asteroids/facade/Facade.java
+++ src/asteroids/facade/Facade.java
@@ -13,33 +13,32 @@ public class Facade implements IFacade {
 
 	@Override
 	public Ship createShip(double x, double y, double xVelocity, double yVelocity, double radius, double orientation) throws ModelException {
-		// TODO Auto-generated method stub
-		return null;
+		Ship ship = new Ship(x, y, xVelocity, yVelocity, c, orientation, radius);
+		return ship;
 	}
 
 	@Override
 	public double[] getShipPosition(Ship ship) throws ModelException {
-		
-		// TODO Auto-generated method stub
-		return null;
+		double[] position = new double[]{ship.getXPosition(), ship.getYPosition()};
+		return position;
 	}
 
 	@Override
 	public double[] getShipVelocity(Ship ship) throws ModelException {
-		// TODO Auto-generated method stub
-		return null;
+		double[] velocity = new double[]{ship.getXVelocity(), ship.getYVelocity()};
+		return velocity;
 	}
 
 	@Override
 	public double getShipRadius(Ship ship) throws ModelException {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+		double radius = ship.getShapeRadius();
+		return radius;
+}
 
 	@Override
 	public double getShipOrientation(Ship ship) throws ModelException {
-		// TODO Auto-generated method stub
-		return 0;
+		double orientation = ship.getOrientation();
+		return orientation;
 	}
 
 	@Override
--- src/asteroids/model/Ship.java
+++ src/asteroids/model/Ship.java
@@ -7,11 +7,19 @@ package asteroids.model;
  * @invar  The y-position of each spaceship must be a valid y-position for any
  *         spaceship.
  *       | isValidYPosition(getYPosition())
+ * @invar  The xVelocity of each Ship must be a valid xVelocity for any
+ *         Ship.
+ *       | isValidXVelocity(getXVelocity())
+ * @invar  The yVelocity of each Ship must be a valid yVelocity for any
+ *         Ship.
+ *       | isValidYVelocity(getYVelocity())
  * @invar  Each spaceship can have its maximum velocity as maximum velocity .
  *       | canHaveAsMaximumVelocity(this.getMaximumVelocity())
  * @invar  The orientation of each spaceship must be a valid orientation for any
  *         spaceship.
  *       | isValidOrientation(getOrientation())
+ * @invar  Each spaceship can have its shapeRadius as shapeRadius.
+ *       | canHaveAsShapeRadius(this.getShapeRadius())
  */
 public class Ship {
 
@@ -50,18 +58,67 @@ public class Ship {
 	 * @post   The orientation of this new spaceship is equal to the given
 	 *         orientation.
 	 *       | new.getOrientation() == orientation
+	 *       
+	 * Initialize this new spaceship with given shapeRadius.
+	 * 
+	 * @param  shapeRadius
+	 *         The shapeRadius for this new spaceship.
+	 * @post   The shapeRadius of this new spaceship is equal to the given
+	 *         shapeRadius.
+	 *       | new.getShapeRadius() == shapeRadius
+	 * @throws InvalidShapeRadiusException
+	 *         This new spaceship cannot have the given shapeRadius as its shapeRadius.
+	 *       | ! canHaveAsShapeRadius(this.getShapeRadius())
+	 *       
+	 * Initialize this new Ship with given yVelocity.
+	 * 
+	 * @param  yVelocity
+	 *         The yVelocity for this new Ship.
+	 * @post   If the given yVelocity is a valid yVelocity for any Ship,
+	 *         the yVelocity of this new Ship is equal to the given
+	 *         yVelocity. Otherwise, the yVelocity of this new Ship is equal
+	 *         to 0.
+	 *       | if (isValidYVelocity(yVelocity))
+	 *       |   then new.getYVelocity() == yVelocity
+	 *       |   else new.getYVelocity() == 0
+	 *       
+	 * Initialize this new Ship with given xVelocity.
+	 * 
+	 * @param  xVelocity
+	 *         The xVelocity for this new Ship.
+	 * @post   If the given xVelocity is a valid xVelocity for any Ship,
+	 *         the xVelocity of this new Ship is equal to the given
+	 *         xVelocity. Otherwise, the xVelocity of this new Ship is equal
+	 *         to 0.
+	 *       | if (isValidXVelocity(xVelocity))
+	 *       |   then new.getXVelocity() == xVelocity
+	 *       |   else new.getXVelocity() == 0
 	 */
 
-	public Ship(double x, double y, double c, double orientation)
-			throws Exception1, Exception2 {
+	public Ship(double x, double y, double xVelocity, double yVelocity, double c, double orientation, double shapeRadius )
+			throws InvalidXException, InvalidYException, InvalidShapeRadiusException {
 		this.setXPosition(x);
 		this.setYPosition(y);
+
+		if (! isValidXVelocity(xVelocity))
+			xVelocity = 0;
+		setXVelocity(xVelocity);
+
+		if (! isValidYVelocity(yVelocity))
+			yVelocity = 0;
+		setYVelocity(yVelocity);
 
 		if (! canHaveAsMaximumVelocity(c))
 			c = 300000;
 		this.c = c;
 
 		this.setOrientation(orientation);
+
+		if (! canHaveAsShapeRadius(shapeRadius))
+			throw new InvalidShapeRadiusException();
+		this.shapeRadius = shapeRadius;
+
+
 
 	}
 
@@ -73,7 +130,7 @@ public class Ship {
 	/**
 	 * Return the x-position of this spaceship.
 	 */
-	@Basic @Raw
+	//	@Basic @Raw
 	public double getXPosition() {
 		return this.x;
 	}
@@ -99,16 +156,16 @@ public class Ship {
 	 * @post   The x-position of this new spaceship is equal to
 	 *         the given x-position.
 	 *       | new.getXPosition() == x
-	 * @throws Exception1
+	 * @throws InvalidXException
 	 *         The given x-position is not a valid x-position for any
 	 *         spaceship.
 	 *       | ! isValidXPosition(getXPosition())
 	 */
-	@Raw
+	//	@Raw
 	public void setXPosition(double x) 
-			throws Exception1 {
+			throws InvalidXException {
 		if (! isValidXPosition(x))
-			throw new Exception1();
+			throw new InvalidXException();
 		this.x = x;
 	}
 
@@ -125,7 +182,7 @@ public class Ship {
 	/**
 	 * Return the y-position of this spaceship.
 	 */
-	@Basic @Raw
+	//	@Basic @Raw
 	public double getYPosition() {
 		return this.y;
 	}
@@ -151,16 +208,16 @@ public class Ship {
 	 * @post   The y-position of this new spaceship is equal to
 	 *         the given y-position.
 	 *       | new.getYPosition() == y
-	 * @throws Exception2
+	 * @throws InvalidYException
 	 *         The given y-position is not a valid y-position for any
 	 *         spaceship.
 	 *       | ! isValidYPosition(getYPosition())
 	 */
-	@Raw
+	//	@Raw
 	public void setYPosition(double y) 
-			throws Exception2 {
+			throws InvalidYException {
 		if (! isValidYPosition(y))
-			throw new Exception2();
+			throw new InvalidYException();
 		this.y = y;
 	}
 
@@ -171,13 +228,116 @@ public class Ship {
 
 
 	//////////////////////////////////////
+	//			X_VELOCITY				// Tot_inmut
+	//////////////////////////////////////
+
+
+
+
+	/**
+	 * Return the xVelocity of this Ship.
+	 */
+	@Basic @Raw
+	public double getXVelocity() {
+		return this.xVelocity;
+	}
+
+	/**
+	 * Check whether the given xVelocity is a valid xVelocity for
+	 * any Ship.
+	 *  
+	 * @param  xVelocity
+	 *         The xVelocity to check.
+	 * @return 
+	 *       | result == Math.sqr(xVelocity^2 + getYVelocity^2) > getMaximumVelocity()
+	 */
+	public static boolean isValidXVelocity(double xVelocity) {
+		return false;
+	}
+
+	/**
+	 * Set the xVelocity of this Ship to the given xVelocity.
+	 * 
+	 * @param  xVelocity
+	 *         The new xVelocity for this Ship.
+	 * @post   If the given xVelocity is a valid xVelocity for any Ship,
+	 *         the xVelocity of this new Ship is equal to the given
+	 *         xVelocity.
+	 *       | if (isValidXVelocity(xVelocity))
+	 *       |   then new.getXVelocity() == xVelocity
+	 */
+	@Raw
+	public void setXVelocity(double xVelocity) {
+		if (isValidXVelocity(xVelocity))
+			this.xVelocity = xVelocity;
+	}
+
+	/**
+	 * Variable registering the xVelocity of this Ship.
+	 */
+	private double xVelocity;
+
+
+
+
+	//////////////////////////////////////
+	//			Y_VELOCITY				// Tot_inmut
+	//////////////////////////////////////
+
+	/**
+	 * Return the yVelocity of this Ship.
+	 */
+	//@Basic @Raw
+	public double getYVelocity() {
+		return this.yVelocity;
+	}
+
+	/**
+	 * Check whether the given yVelocity is a valid yVelocity for
+	 * any Ship.
+	 *  
+	 * @param  yVelocity
+	 *         The yVelocity to check.
+	 * @return 
+	 *       | result == Math.sqr(getXVelocity^2 + yVelocity) > getMaximumVelocity()
+	 */
+	public static boolean isValidYVelocity(double yVelocity) {
+		return false;
+	}
+
+	/**
+	 * Set the yVelocity of this Ship to the given yVelocity.
+	 * 
+	 * @param  yVelocity
+	 *         The new yVelocity for this Ship.
+	 * @post   If the given yVelocity is a valid yVelocity for any Ship,
+	 *         the yVelocity of this new Ship is equal to the given
+	 *         yVelocity.
+	 *       | if (isValidYVelocity(yVelocity))
+	 *       |   then new.getYVelocity() == yVelocity
+	 */
+	//@Raw
+	public void setYVelocity(double yVelocity) {
+		if (isValidYVelocity(yVelocity))
+			this.yVelocity = yVelocity;
+	}
+
+	/**
+	 * Variable registering the yVelocity of this Ship.
+	 */
+	private double yVelocity;
+
+
+	//////////////////////////////////////
 	//			MAXIMUM VELOCITY		// Tot_inmut
 	//////////////////////////////////////
+
+
 
 	/**
 	 * Return the maximum velocity of this spaceship.
 	 */
-	@Basic @Raw @Immutable
+	//	@Basic @Raw @Immutable
 	public double getMaximumVelocity() {
 		return this.c;
 	}
@@ -190,7 +350,7 @@ public class Ship {
 	 * @return 
 	 *       | result == (c >= 0 && c<= 300000)
 	 */
-	@Raw
+	//	@Raw
 	public boolean canHaveAsMaximumVelocity(double c) {
 		return false;
 	}
@@ -208,7 +368,7 @@ public class Ship {
 	/**
 	 * Return the orientation of this spaceship.
 	 */
-	@Basic @Raw
+	//	@Basic @Raw
 	public double getOrientation() {
 		return this.orientation;
 	}
@@ -238,7 +398,7 @@ public class Ship {
 	 *         orientation.
 	 *       | new.getOrientation() == orientation
 	 */
-	@Raw
+	//	@Raw
 	public void setOrientation(double orientation) {
 		assert isValidOrientation(orientation);
 		this.orientation = orientation;
@@ -248,58 +408,36 @@ public class Ship {
 	 * Variable registering the orientation of this spaceship.
 	 */
 	private double orientation;
-	
-	
+
+
 	//////////////////////////////////////
-	//			ORIENTATION				// Def_inmut
+	//			SHAPE_RADIUS			// Def_inmut
 	//////////////////////////////////////
 
-	/** TO BE ADDED TO CLASS HEADING
-	 * @invar  Each spaceship can have its shapeRadius as shapeRadius.
-	 *       | canHaveAsShapeRadius(this.getShapeRadius())
+
+	/**
+	 * Return the shapeRadius of this spaceship.
 	 */
+	//	@Basic @Raw @Immutable
+	public double getShapeRadius() {
+		return this.shapeRadius;
+	}
 
-/**
- * Initialize this new spaceship with given shapeRadius.
- * 
- * @param  shapeRadius
- *         The shapeRadius for this new spaceship.
- * @post   The shapeRadius of this new spaceship is equal to the given
- *         shapeRadius.
- *       | new.getShapeRadius() == shapeRadius
- * @throws Exception3
- *         This new spaceship cannot have the given shapeRadius as its shapeRadius.
- *       | ! canHaveAsShapeRadius(this.getShapeRadius())
- */
-public Ship(double shapeRadius) throws Exception3 {
-	if (! canHaveAsShapeRadius(shapeRadius))
-		throw new Exception3();
-	this.shapeRadius = shapeRadius;
-}
+	/**
+	 * Check whether this spaceship can have the given shapeRadius as its shapeRadius.
+	 *  
+	 * @param  shapeRadius
+	 *         The shapeRadius to check.
+	 * @return 
+	 *       | result == (10 <= shapeRadius)
+	 */
+	//	@Raw
+	public boolean canHaveAsShapeRadius(double shapeRadius) {
+		return false;
+	}
 
-/**
- * Return the shapeRadius of this spaceship.
- */
-@Basic @Raw @Immutable
-public double getShapeRadius() {
-	return this.shapeRadius;
-}
-
-/**
- * Check whether this spaceship can have the given shapeRadius as its shapeRadius.
- *  
- * @param  shapeRadius
- *         The shapeRadius to check.
- * @return 
- *       | result == (10 <= shapeRadius)
-*/
-@Raw
-public boolean canHaveAsShapeRadius(double shapeRadius) {
-	return false;
-}
-
-/**
- * Variable registering the shapeRadius of this spaceship.
- */
-private final double shapeRadius;
+	/**
+	 * Variable registering the shapeRadius of this spaceship.
+	 */
+	private final double shapeRadius;
 }
